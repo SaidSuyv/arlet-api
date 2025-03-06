9e53551a66c29761a412920a17216e5e40d2a8c4
SaidSuyv <sirsuyvi@gmail.com>
2025-03-06 13:54:53 -0500

    update product.php

--- res/product.php
+++ res/product.php
@@ -11,6 +11,36 @@ class cProduct extends cArlet
     $this->cAuth = new cAuth();
   }
 
-  
+  public function f_add_product($data)
+  {
+    if( $this->cAuth->f_check_token() )
+    {
+      $params = [
+        "code",
+        "ptype",
+        "ipath",
+        "desc",
+        "cprice",
+        "profit",
+        "csale",
+        "whprice",
+        "whminq",
+        "flg_inv",
+        "stock",
+        "min_st",
+        "xpdate",
+        "sunat_unit",
+        "sunat_pcode",
+        "igv_per",
+        "icbper_am"
+      ];
+      if( $this->check_params( $data , $params ) )
+      {
+        
+      }
+      else $this->set_error("Server Error: P001");
+    }
+    else $this->set_error("Authentication failed");
+  }
 
 }
